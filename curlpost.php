31e61ce2f8d80341466be785dbff3c5790e70ef2
tonymarion <tony.marion@amdocs.com>
2016-11-15 17:01:10 -0600

    Create curlpost.php

---
+++ curlpost.php
@@ -0,0 +1,30 @@
+<?php
+//
+// A very simple PHP example that sends a HTTP POST to a remote site
+//
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"https://api.projectoxford.ai/vision/v1.0/analyze?visualFeatures=Categories&language=en");
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                                            'Content-Type: application/json',
+                                            'Connection: Keep-Alive',
+                                            'Ocp-Apim-Subscription-Key: 7de0d688-ff48-4516-9209-7a45e179bbc6'
+                                            ));
+curl_setopt($ch, CURLOPT_POST, 1);
+//curl_setopt($ch, CURLOPT_POSTFIELDS,
+//            "postvar1=value1&postvar2=value2&postvar3=value3");
+
+// in real life you should use something like:
+// curl_setopt($ch, CURLOPT_POSTFIELDS, 
+//          http_build_query(array('postvar1' => 'value1')));
+
+// receive server response ...
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+
+?>
