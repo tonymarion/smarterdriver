0b690ab967097926127cdc6d714457b2757e295a
tonymarion <tony.marion@amdocs.com>
2016-11-15 17:30:43 -0600

    Update curlpostv3.php

--- curlpostv3.php
+++ curlpostv3.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = 'http://sms2.cdyne.com/sms.svc/SimpleSMSsendWithPostback?        PhoneNumber=18887477474&Message=test&LicenseKey=LICENSEKEY';
+$url = 'https://api.projectoxford.ai/vision/v1.0/analyze?visualFeatures=Categories&language=en';
 
 $cURL = curl_init();
 
@@ -9,14 +9,14 @@ curl_setopt($cURL, CURLOPT_HTTPGET, true);
 
 curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json',
-    'Accept: application/json'
+    'Accept: application/json',
+    'Ocp-Apim-Subscription-Key: 7de0d688-ff48-4516-9209-7a45e179bbc6',
+    'url: http://www.lessons4living.com/images/penclchk.gif'
 ));
 
 $result = curl_exec($cURL);
 
 curl_close($cURL);
-
-
 
 print_r($result);
 
