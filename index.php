cd3a8c5a82006ec3613fb1e48ee45f4c1106ce52
tonymarion <tony.marion@amdocs.com>
2016-11-15 16:04:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,40 @@
 <?php
+// This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
+require_once 'HTTP/Request2.php';
 
-echo "1234";
+$request = new Http_Request2('https://api.projectoxford.ai/vision/v1.0/analyze');
+$url = $request->getUrl();
+
+$headers = array(
+    // Request headers
+    'Content-Type' => 'application/json',
+    'Ocp-Apim-Subscription-Key' => '7de0d688-ff48-4516-9209-7a45e179bbc6',
+);
+
+$request->setHeader($headers);
+
+$parameters = array(
+    // Request parameters
+    'visualFeatures' => 'Categories',
+    'details' => '{string}',
+    'language' => 'en',
+);
+
+$url->setQueryVariables($parameters);
+
+$request->setMethod(HTTP_Request2::METHOD_POST);
+
+// Request body
+$request->setBody("{body}");
+
+try
+{
+    $response = $request->send();
+    echo $response->getBody();
+}
+catch (HttpException $ex)
+{
+    echo $ex;
+}
+
 ?>
